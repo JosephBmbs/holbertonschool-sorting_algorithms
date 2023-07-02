86190bf85e493f8aa1dcccc1f61fc33ad3e77e51
Youssef_Saad <6270@holbertonstudents.com>
2023-07-02 17:35:50 +0100

    fixing betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 
-
 /**
  * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
  * @array: Pointer to the array to be sorted
@@ -8,29 +7,30 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    bool swapped;
+	size_t i, j;
+	int temp;
+	bool swapped;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = false;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = true;
-                print_array(array, size);
-            }
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = true;
+				print_array(array, size);
+			}
+		}
 
-        if (!swapped)
-            break;
-    }
+		if (!swapped)
+			break;
+	}
 }
+
